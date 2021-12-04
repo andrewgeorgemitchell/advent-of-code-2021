8ce5f266f2d8922f2f178ecdfcdfa82755b65158
Andrew <andrewgeorgemitchell@gmail.com>
2021-12-03 19:54:54 -0800

    Complete Day 03

---
+++ day-03/day-03.test.ts
@@ -0,0 +1,98 @@
+import {
+  calculateGammaEpsilonNumbers,
+  charFrequencyForCol,
+  convertBinaryToBits,
+  calculateRating,
+  O2RateComparator,
+  C02RateComparator,
+  convertBinaryArrayToDecimal,
+} from "./day-03";
+
+const testInputFileString = `00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010`;
+
+const testOutput = [
+  [0, 0, 1, 0, 0],
+  [1, 1, 1, 1, 0],
+  [1, 0, 1, 1, 0],
+  [1, 0, 1, 1, 1],
+  [1, 0, 1, 0, 1],
+  [0, 1, 1, 1, 1],
+  [0, 0, 1, 1, 1],
+  [1, 1, 1, 0, 0],
+  [1, 0, 0, 0, 0],
+  [1, 1, 0, 0, 1],
+  [0, 0, 0, 1, 0],
+  [0, 1, 0, 1, 0],
+];
+
+describe("Day 03", () => {
+  test("file conversion", () => {
+    const output = convertBinaryToBits(testInputFileString);
+    expect(output).toEqual(testOutput);
+  });
+
+  test("charFrequency for column", () => {
+    expect(charFrequencyForCol(0, testOutput)).toEqual({ 0: 5, 1: 7 });
+  });
+
+  test("convertBinaryArrayToDecimal", () => {
+    expect(convertBinaryArrayToDecimal([0, 1, 0, 1, 0])).toBe(10);
+  });
+
+  test("calculateGammaEpsilonNumbers works as expected", () => {
+    const { gamma, epsilon } = calculateGammaEpsilonNumbers(testOutput);
+    expect(gamma).toEqual([1, 0, 1, 1, 0]);
+    expect(epsilon).toEqual([0, 1, 0, 0, 1]);
+  });
+
+  test("calculateRating for O2", () => {
+    const result = calculateRating({
+      col: 0,
+      matrix: testOutput,
+      comparator: ({
+        zeroTotal,
+        oneTotal,
+      }: {
+        zeroTotal: number;
+        oneTotal: number;
+      }): number => {
+        if (zeroTotal > oneTotal) {
+          return 0;
+        } else if (zeroTotal < oneTotal) {
+          return 1;
+        } else {
+          return 1;
+        }
+      },
+    });
+    expect(result).toEqual([1, 0, 1, 1, 1]);
+  });
+
+  test("calculateRating for O2", () => {
+    const result = calculateRating({
+      matrix: testOutput,
+      comparator: O2RateComparator,
+    });
+    expect(result).toEqual([1, 0, 1, 1, 1]);
+  });
+
+  test("calculateRating for C02", () => {
+    const co2Expected = [0, 1, 0, 1, 0];
+    const result = calculateRating({
+      matrix: testOutput,
+      comparator: C02RateComparator,
+    });
+    expect(result).toEqual(co2Expected);
+  });
+});
---
+++ day-03/day-03.ts
@@ -0,0 +1,143 @@
+import fs from "fs";
+
+const fileString = fs.readFileSync(__dirname + "/day-03.data.txt").toString();
+
+export const convertBinaryToBits = (
+  fileString: string
+): Array<Array<number>> => {
+  return fileString.split("\n").map((row: string) => {
+    return row.split("").map((bit: string) => parseInt(bit));
+  });
+};
+
+export const charFrequencyForCol = (
+  col: number,
+  matrix: Array<Array<number>>
+): Record<number, number> => {
+  const frequency: Record<number, number> = {};
+  for (let i = 0; i < matrix.length; i++) {
+    const current = matrix[i][col];
+    if (current in frequency) {
+      frequency[current]++;
+    } else {
+      frequency[current] = 1;
+    }
+  }
+  return frequency;
+};
+
+export const convertBinaryArrayToDecimal = (arr: Array<number>): number =>
+  parseInt(arr.join(""), 2);
+// Part 1
+
+export const calculateGammaEpsilonNumbers = (
+  bits: Array<Array<number>>
+): { gamma: Array<number>; epsilon: Array<number> } => {
+  const gamma = [];
+  const epsilon = [];
+  for (let i = 0; i < bits[0].length; i++) {
+    const { 0: zeros, 1: ones } = charFrequencyForCol(i, bits);
+    if (ones > zeros) {
+      gamma.push(1);
+      epsilon.push(0);
+    } else {
+      gamma.push(0);
+      epsilon.push(1);
+    }
+  }
+  return { gamma, epsilon };
+};
+
+console.log("Part 1:");
+const { gamma, epsilon } = calculateGammaEpsilonNumbers(
+  convertBinaryToBits(fileString)
+);
+const gammaValue = convertBinaryArrayToDecimal(gamma);
+const epsilonValue = convertBinaryArrayToDecimal(epsilon);
+console.log("{gamma, epsilon}: ", { gamma, epsilon });
+console.log("Gamma Decimal: ", gammaValue);
+console.log("Epsilon Decimal: ", epsilonValue);
+console.log("product: ", epsilonValue * gammaValue);
+
+// Part 2
+
+export const calculateRating = ({
+  col = 0,
+  matrix,
+  comparator,
+}: {
+  col?: number;
+  matrix: Array<Array<number>>;
+  comparator: ({
+    zeroTotal,
+    oneTotal,
+  }: {
+    zeroTotal: number;
+    oneTotal: number;
+  }) => number;
+}): number[] => {
+  if (matrix.length === 1) {
+    return matrix[0];
+  }
+
+  const chars = charFrequencyForCol(col, matrix)
+
+  const compareValue = comparator({ zeroTotal: chars[0], oneTotal: chars[1] });
+
+  const newMatrix = matrix.filter((arr) => arr[col] === compareValue);
+
+  return calculateRating({
+    col: col + 1,
+    matrix: newMatrix,
+    comparator,
+  });
+};
+
+export const O2RateComparator = ({
+  zeroTotal,
+  oneTotal,
+}: {
+  zeroTotal: number;
+  oneTotal: number;
+}): number => {
+  if (zeroTotal > oneTotal) {
+    return 0;
+  } else if (zeroTotal < oneTotal) {
+    return 1;
+  } else {
+    return 1;
+  }
+};
+
+export const C02RateComparator = ({
+  zeroTotal,
+  oneTotal,
+}: {
+  zeroTotal: number;
+  oneTotal: number;
+}): number => {
+  if (zeroTotal > oneTotal) {
+    return 1;
+  } else if (zeroTotal < oneTotal) {
+    return 0;
+  } else {
+    return 0;
+  }
+};
+
+console.log("Part 2:");
+const O2Rating = convertBinaryArrayToDecimal(
+  calculateRating({
+    matrix: convertBinaryToBits(fileString),
+    comparator: O2RateComparator,
+  })
+);
+const C02Rating = convertBinaryArrayToDecimal(
+  calculateRating({
+    matrix: convertBinaryToBits(fileString),
+    comparator: C02RateComparator,
+  })
+);
+console.log("O2Rating:", O2Rating);
+console.log("C02Rating:", C02Rating);
+console.log("product: ", O2Rating * C02Rating);
